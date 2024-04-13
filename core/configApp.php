52ada006887cb36f4a724e4de0f04799e586d0df
Oscarsp15 <oscar.sirlopu@gmail.com>
2024-04-13 00:57:31 -0500

    mi primer commit

---
+++ core/configApp.php
@@ -0,0 +1,9 @@
+<?php
+    const SERVER = "localhost";
+    const DB = "amodeus";
+    const USER = "root";
+    const PASS = ""; 
+    const SGDB = "mysql:host=".SERVER.";dbname=".DB;
+    const SECRET_KEY = '$AMOSIS@2020';
+    const SECRET_IV = '201901';
+    const METHOD = "AES-256-CBC";
---
+++ core/configGeneral.php
@@ -0,0 +1,5 @@
+<?php
+    const SERVERURL = "http://192.168.196.94/CODIGO%20FUENTE/";
+    const COMPANY = "STARTUP[2020]"; 
+    const MONEDA = "S/";
+    const ZONE = "America/Guatemala";
